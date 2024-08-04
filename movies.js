9b22bd142855a6d93c465c5c9f83f593a51e5618
dajung <kwonkkan@naver.com>
2024-08-04 17:33:01 +0900

    개봉 예정작 슬라이드 오류 수정

--- movies.js
+++ movies.js
@@ -2,7 +2,9 @@
 // 1) 최초에 화면(html)이 로드되면, TMDB로부터 데이터를 쭉 갖고 온다.
 // 2) 쭉 갖고오면, 개수를 읽어서 슬라이드를 만든다.
 // 3) 슬라이드의 가로 길이를 구한다.
-// 4)
+// 4) 슬라이드의 첫 시작점 위치를 구한다.
+// 5) 버튼을 누를 수록 슬라이드 카운트가 올라간다.
+// 6) 슬라이드의 인덱스와 카운트 값이 같아지면 처음 위치로 돌아간다.
 
 // 2) 시간순에 맞게 재배치
 
@@ -20,8 +22,7 @@ const popularUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY
 const topUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=ko&page=1&region=KR`;
 const nowUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=ko&page=1&region=KR`;
 
-// 검색
-// 윈도우가 준비됐을 때 해당 함수 실행
+// 검색: 윈도우가 준비됐을 때 해당 함수 실행
 window.onload = function () {
   document.getElementById("search-button").addEventListener("click", () => {
     const query = document.getElementById("search-input").value.toLowerCase();
@@ -62,66 +63,18 @@ fetch(upcomingUrl, options)
     });
 
     // 여기
-    let slides = document.querySelector(".Container1");
-    let slide = document.querySelectorAll(".Container1 .upcomingPoster");
+    let slides = document.querySelector(".upcomingList");
+    let slide = document.querySelectorAll(".upcomingList .upcomingPoster");
     let currentIdx = 0;
     let slideCount = slide.length;
     let slideWidth = 300; // 슬라이드를 얼만큼 넘길 건지(1개씩 넘길 거면 300)
     let slideMargin = 30;
-    prevBtn = document.querySelector(".prev");
-    nextBtn = document.querySelector(".next");
+    let prevBtn = document.querySelector(".prev");
+    let nextBtn = document.querySelector(".next");
 
-    console.log("수정중 => ", slide);
+    console.log("수정중 => ", slideCount);
 
-    // 전체 너비를 구해서 ul의 너비를 지정하는 함수
-    function updateWidth() {
-      let currentSlides = document.querySelectorAll(".Container1 .upcomingPoster");
-      let newSlideCount = currentSlides.length;
-
-      let newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + "px";
-      slides.style.width = newWidth;
-    }
-
-    //초기 위치 잡는 함수
-    function setinit() {
-      // 왼쪽으로 움직일거니까 ( - )붙임
-      // 이동할 변수
-      let TranslateValue = -(slideWidth + slideMargin) * slideCount;
-      slides.style.transform = "translateX(" + TranslateValue + "px)";
-    }
-    nextBtn.addEventListener("click", function () {
-      // 지금 보고있는 슬라이드 수 +1 로 이동
-      moveSlide(currentIdx + 1);
-      // console.log("인덱스 => " + currentIdx);
-    });
-    prevBtn.addEventListener("click", function () {
-      // 지금 보고있는 슬라이드 수 -1 로 이동
-      moveSlide(currentIdx - 1);
-    });
-
-    function moveSlide(num) {
-      // 원래는 0이었는데 사용자가 이동하면 index가 1로 바뀌어있어야 함
-      slides.style.left = -num * (slideWidth + slideMargin) + "px";
-      //이동한 다음에는 currentIdx를 반드시 슬라이드가 최종적으로 보고있는 num 숫자만큼 바껴있어야 함.
-      currentIdx = num;
-      console.log(currentIdx, slideCount);
-      // 마지막이면 1번으로 다시 돌리기
-
-      if (currentIdx == slideCount || currentIdx == -slideCount) {
-        setTimeout(function () {
-          slides.classList.remove("animated");
-          slides.style.left = "0px";
-          currentIdx = 0;
-        }, 500);
-        console.log("확인"); // 이게 안 뜸 if문 작동 안 하는 것
-
-        setTimeout(function () {
-          slides.classList.add("animated");
-        }, 600);
-      }
-    }
-
-    makeClone();
+  
 
     function makeClone() {
       for (let i = 0; i < slideCount; i++) {
@@ -143,6 +96,53 @@ fetch(upcomingUrl, options)
       setTimeout(function () {
         slides.classList.add("animated");
       }, 100);
+      makeClone();
+    }
+
+    // 전체 너비를 구해서 포스터의 너비를 지정하는 함수
+    function updateWidth() {
+      let currentSlides = document.querySelectorAll(".upcomingList .upcomingPoster");
+      let newSlideCount = currentSlides.length;
+      let newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + "px";
+      slides.style.width = newWidth;
+    }
+
+    //초기 위치 잡는 함수
+    function setinit() {
+      // 왼쪽으로 움직일거니까 ( - )붙임
+      // 이동할 변수
+      let TranslateValue = -(slideWidth + slideMargin) * slideCount;
+      slides.style.transform = "translateX(" + TranslateValue + "px)";
+    }
+    nextBtn.addEventListener("click", function () {
+      // 지금 보고있는 슬라이드 수 +1 로 이동
+      moveSlide(currentIdx + 1);
+    });
+    prevBtn.addEventListener("click", function () {
+      // 지금 보고있는 슬라이드 수 -1 로 이동
+      moveSlide(currentIdx - 1);
+    });
+
+    function moveSlide(num) {
+      // 원래는 0이었는데 사용자가 이동하면 index가 1로 바뀌어있어야 함
+      slides.style.left = -num * (slideWidth + slideMargin) + "px";
+      //이동한 다음에는 currentIdx를 반드시 슬라이드가 최종적으로 보고있는 num 숫자만큼 바껴있어야 함.
+      currentIdx = num;
+      console.log(currentIdx, slideCount);
+
+      // 마지막이면 1번으로 다시 돌리기
+      if (currentIdx == slideCount || currentIdx == -slideCount) {
+        setTimeout(function () {
+          slides.classList.remove("animated");
+          slides.style.left = "0px";
+          currentIdx = 0;
+        }, 500);
+        console.log("확인"); // 이게 안 뜸 if문 작동 안 하는 것
+
+        setTimeout(function () {
+          slides.classList.add("animated");
+        }, 600);
+      }
     }
   })
   .catch((err) => console.error(err));
@@ -174,9 +174,9 @@ document.getElementById("top-btn").addEventListener("click", function () {
   });
 });
 
-// 개봉 예정작 슬라이드
-// let slides = document.querySelector(".Container1"),
-//   slide = document.querySelectorAll(".Container1 .upcomingPoster"),
+// // 개봉 예정작 슬라이드
+// let slides = document.querySelector(".container1"),
+//   slide = document.querySelectorAll(".container1 .upcomingPoster"),
 //   currentIdx = 0,
 //   slideCount = slide.length,
 //   slideWidth = 900, // 슬라이드를 얼만큼 넘길 건지(1개씩 넘길 거면 300)
@@ -210,7 +210,7 @@ document.getElementById("top-btn").addEventListener("click", function () {
 
 // // 전체 너비를 구해서 ul의 너비를 지정하는 함수
 // function updateWidth() {
-//   let currentSlides = document.querySelectorAll(".Container1 .upcomingPoster");
+//   let currentSlides = document.querySelectorAll(".container1 .upcomingPoster");
 //   let newSlideCount = currentSlides.length;
 
 //   let newWidth = (slideWidth + slideMargin) * newSlideCount - slideMargin + "px";
@@ -235,9 +235,9 @@ document.getElementById("top-btn").addEventListener("click", function () {
 //   moveSlide(currentIdx - 1);
 // });
 
-// 숫자가 넘어와야 함수가 작동하도록
-// next 누를수록 왼쪽으로 translate left 값이 거리만큼 이동해야함
-// 전체가 슬라이드 너비+여백만큼 이동해야함
+// // 숫자가 넘어와야 함수가 작동하도록
+// // next 누를수록 왼쪽으로 translate left 값이 거리만큼 이동해야함
+// // 전체가 슬라이드 너비+여백만큼 이동해야함
 // function moveSlide(num) {
 //   // 원래는 0이었는데 사용자가 이동하면 index가 1로 바뀌어있어야 함
 //   slides.style.left = -num * (slideWidth + slideMargin) + "px";
